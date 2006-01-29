578177124c711c908ebd46ecbea626ebd621b0b8
ivo <ivo@6043272b-7505-0410-b453-f90717318246>
2006-01-29 12:51:49 +0000

    initial import
    git-svn-id: file:///home/ivo/tmp/oud/opensource/svn/python/mollie/trunk@16 6043272b-7505-0410-b453-f90717318246

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+
+import os, sys
+from glob import fnmatch
+
+from mollie.version import version
+
+ignore = ('CVS', )
+        
+setup(name = "mollie", 
+      version = version,
+      maintainer = 'Ivo van der Wijk',
+      maintainer_email = 'ivo@m3r.nl',
+      author = 'Ivo van der Wijk',
+      author_email = 'ivo@m3r.nl',
+      platforms = ['any'],
+      url = 'http://projects.m3r.nl/opensource/trac/mollie',
+      description = 'Mollie.nl SMS gateway API',
+      long_description = 
+"""An API Implementation for the mollie.nl SMS Gateway""",
+      license = "BSD License",
+      packages = ["mollie"],
+)
